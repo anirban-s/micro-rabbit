cae481b723b8a31cc3779e3ef00e909f1ca495ad
anirban-s <baban.chrome@gmail.com>
2021-06-04 00:34:52 +0530

    Partially completed banking micro service

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDBContext.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDBContext : DbContext
+    {
+        public BankingDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
